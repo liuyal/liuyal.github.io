9b2548b1e4781d15060506bc32487045eb3a6e38
Jerry Liu <jerrylarryliu@gmail.com>
2020-10-16 12:26:50 -0700

    added dynamic tabs

--- pages/gallary/js/gallary.js
+++ pages/gallary/js/gallary.js
@@ -60,29 +60,54 @@ function request_github_data() {
                     if (!(month in dates[year])) {
                         dates[year][month] = {};
                     }
-                    
+
                     dates[year][month][date_label].push(raw_url + '/' + data[i]["path"]);
                 }
             }
         }
+
+        dates["2021"] = {};
+        dates["2022"] = {};
+
+        var years = Object.keys(dates);
+        var i;
+        for (i = 0; i < years.length; i++) {
+
+            if (i == 0) {
+
+                document.getElementById("default_tab").innerHTML = years[0];
+                document.getElementById("default_tab").href = "#" + years[0];
+
+            } else {
+
+                var li_node = document.createElement("LI");
+                var clone = document.getElementById("default_tab");
+                var pill_node = clone.cloneNode(true);
+                
+                pill_node.innerHTML = years[i];
+                pill_node.id = years[i];
+                pill_node.href = "#" + years[i];
+
+                li_node.appendChild(pill_node);
+
+                document.getElementById("nav_pills").appendChild(li_node);
+
+            }
+
+             console.log(years[i]);
+        }
+
+       
+
+
     });
 
 }
 
 function check_data() {
-    
-    
     console.log(dates["2020"]);
-    
 }
 
-
-
-
-
-
-
-
-
-
-
+function load_months() {
+    console.log(dates["2020"]);
+}
