6b47e4f36b604d63df0af81fd11c6d7633423e5f
Jerry Liu <jerrylarryliu@gmail.com>
2021-01-12 10:19:05 -0800

    fixed image layout

--- pages/gallery/js/gallery.js
+++ pages/gallery/js/gallery.js
@@ -9,7 +9,6 @@ function doCORSRequest(options, printResult) {
     x.onload = x.onerror = function () {
         printResult(x.responseText);
     };
-
     if (/^POST/i.test(options.method)) {
         x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     }
@@ -20,7 +19,6 @@ function doCORSRequest(options, printResult) {
 function request_github_data() {
 
     let raw_url = "https://raw.githubusercontent.com/liuyal/SpotLightSaver/master/";
-//    let raw_url = "https://gitlab.com/liuyal/SpotLightSaver/-/raw/master/";
     let repo_tree_url = "https://api.github.com/repos/liuyal/SpotLightSaver/git/trees/master?recursive=1";
 
     doCORSRequest({
@@ -40,12 +38,11 @@ function request_github_data() {
                 var day = data[i]["path"].slice(-2);
 
                 if (!(year in global_dates)) {
-                    global_dates[year] = {}; 
+                    global_dates[year] = {};
                 }
                 if (!(month in global_dates[year])) {
                     global_dates[year][month] = {};
                 }
-
                 global_dates[year][month][data[i]["path"]] = [];
 
             } else {
@@ -63,7 +60,6 @@ function request_github_data() {
                     if (!(month in global_dates[year])) {
                         global_dates[year][month] = {};
                     }
-
                     global_dates[year][month][date_label].push(raw_url + '/' + data[i]["path"]);
                 }
             }
@@ -74,12 +70,10 @@ function request_github_data() {
         for (i = 0; i < years.length; i++) {
 
             if (i == 0) {
-
                 document.getElementById("li_0").id = "li_" + years[0];
                 document.getElementById("tab_0").innerHTML = years[0];
                 document.getElementById("tab_0").href = "#" + years[0];
                 document.getElementById("tab_0").classList.add("active");
-
             } else {
 
                 var li_node = document.createElement("LI");
@@ -96,11 +90,9 @@ function request_github_data() {
                 li_node.onclick = function () {
                     load_months(this.id)
                 };
-
                 document.getElementById("nav_pills").appendChild(li_node);
             }
         }
-
         document.getElementById("tab_0").id = "tab_" + years[0];
         load_months("li_" + years[0]);
     });
@@ -115,25 +107,30 @@ function check_data() {
 function load_data() {
 
     images = [];
+    images_pi = [];
     global_month = event.target.id.split("m")[1];
     month = event.target.id.split("m")[1];
     year = this.global_year.split("_")[1];
     data = this.global_dates[year][month];
-
     date_labels = Object.keys(data);
 
     for (i = 0; i < Object.keys(data).length; i++) {
         day_images = data[date_labels[i]];
         for (x in day_images) {
-            images.push(day_images[x]);
-        }
+            image = day_images[x];
 
+            if (image.search("_pi") > -1) {
+                images_pi.push(day_images[x]);
+            } else {
+                images.push(day_images[x]);
+            }
+        }
     }
 
     localStorage.setItem("local_month", global_month);
     localStorage.setItem("local_year", global_year);
     localStorage.setItem("local_images", images);
-
+    localStorage.setItem("local_images_pi", images_pi);
 }
 
 
@@ -142,17 +139,43 @@ function load_images() {
     month = localStorage.getItem("local_month");
     year = localStorage.getItem("local_year").split("_")[1];
     images = localStorage.getItem("local_images").split(",");
+    images_pi = localStorage.getItem("local_images_pi").split(",");
 
-    for (i = 0; i < images.length; i++) {
-        
+    var counter = 0;
+    var im_index = 0
+    var flag = true;
+
+    for (i = 0; i < images.length * 2; i++) {
+
         var temp_image = document.createElement("IMG");
-        var col_element = document.getElementById("col_" + (i%4 + 1));
-        
-        temp_image.src = images[i];
         temp_image.style = "width:100%";
+
+        if (counter > 3) {
+            counter = 0;
+            flag = !flag;
+        }
+        
+        if (flag) {
+            if (i % 2 > 0) {
+                temp_image.src = images_pi[im_index];
+                im_index += 1;
+
+            } else {
+                temp_image.src = images[im_index];
+            }
+
+        } else {
+            if (i % 2 > 0) {
+                temp_image.src = images[im_index];
+                im_index += 1;
+            } else {
+                temp_image.src = images_pi[im_index];
+            }
+        }
+        counter += 1;
+        var col_element = document.getElementById("col_" + (i % 4 + 1));
         col_element.appendChild(temp_image);
     }
-
 }
 
 
@@ -178,12 +201,10 @@ function load_months(year) {
             box.addEventListener('click', function (event) {
                 load_data();
                 window.location.replace("./gallery_month.html");
-
             });
 
         } else {
             box.style.backgroundColor = "LightGray";
-
         }
         box.textContent = "";
     }
